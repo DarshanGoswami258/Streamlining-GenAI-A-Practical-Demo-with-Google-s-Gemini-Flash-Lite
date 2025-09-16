fd5e5c77da96840f702267baa22303a6e075dd6a
DARSHAN GOSWAMI <darshangoswami258@gmail.com>
2025-09-16 21:07:29 +0530

    Update App.py

--- App.py
+++ App.py
@@ -11,7 +11,7 @@ genai.configure(api_key=key)
 model = genai.GenerativeModel("gemini-2.5-flash-lite")
 
 # create a frontend UI
-st.title('SIMPLE TEXT GENRATION')
+st.title('SIMPLE TEXT GENERATION')
 st.header('This to test the gemini model as application')
 prompt = st.text_input('Write your prompt here', max_chars=200)
 if prompt:
